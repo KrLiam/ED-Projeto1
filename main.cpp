88a285764d61d1ffcfa8fd0cd8649a5d2b16f605
William Kraus <88288206+KrLiam@users.noreply.github.com>
2023-04-22 18:20:33 -0300

    add parsing data processing

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "array_stack.h"
 #include "parser.h"
 
+using namespace structures;
+
 std::string read_file() {
     char xmlfilename[100];
 
@@ -48,16 +50,19 @@ struct Cenario {
 };
 
 
-void parse_cenario(StringReader& r) {
+bool* parse_matriz(std::string& value) {
+    StringReader r(value);
 
+    return new bool[10];
 }
 
-void parse(std::string& entrada) {
+ArrayList<Cenario>* parse(std::string& entrada) {
     StringReader r(entrada);
 
-    auto cenarios = new structures::ArrayList<Cenario>(200);
+    auto cenarios = new ArrayList<Cenario>(200);
+    Cenario cenario;
 
-    structures::ArrayStack<std::string> stack(100);
+    ArrayStack<std::string> stack(100);
 
     while (r.peek()) {
         std::string contents = r.read_until('<');
@@ -65,25 +70,55 @@ void parse(std::string& entrada) {
         if (!r.peek()) break;
         StringReader::Tag tag = r.parse_tag();
         
-        if (tag.is_closing) {
+        if (!tag.is_closing) {
+            // opening
+            stack.push(tag.key);
+
+            if (tag.key == "cenario") {
+                cenario = Cenario();
+            }
+        } else {
+            // closing
             std::string current = stack.pop();
             if (current != tag.key) {
                 throw parse_error("Unclosed tag.");
             }
-        } else {
-            stack.push(tag.key);
+
+            if (tag.key == "cenario") {
+                cenarios->push_back(cenario);
+            }
+            else if (tag.key == "nome") {
+                cenario.nome = contents;
+            }
+            else if (tag.key == "x") {
+                cenario.roboX = std::stoi(contents);
+            }
+            else if (tag.key == "y") {
+                cenario.roboX = std::stoi(contents);
+            }
+            else if (tag.key == "largura") {
+                cenario.largura = std::stoi(contents);
+            }
+            else if (tag.key == "altura") {
+                cenario.altura = std::stoi(contents);
+            }
+            else if (tag.key == "matriz") {
+                cenario.matriz = parse_matriz(contents);
+            }
         }
     }
 
     if (stack.size()) {
         throw parse_error("Unclosed tag.");
     }
+
+    return cenarios;
 }
 
 int main() {
     std::string entrada = read_file();
 
-    parse(entrada);
+    ArrayList<Cenario>* cenarios = parse(entrada);
 
     return 0;
 }
